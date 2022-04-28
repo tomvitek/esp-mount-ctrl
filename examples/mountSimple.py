efff2323a46de08f23b2b5451f38f4522eafa073
Tomas Vitek <tomas.vitek.tvt@gmail.com>
2022-04-28 15:01:23 +0200

    Documented mountSimple example

--- examples/mountSimple.py
+++ examples/mountSimple.py
@@ -3,6 +3,7 @@ from astropy.coordinates import SkyCoord, EarthLocation
 import astropy.units as u
 from astropy.time import Time
 
+# SETUP MOUNT
 location = EarthLocation.from_geodetic(48, 16)
 axCoord = SkyCoord(alt=30 * u.degree, az=270 * u.degree, frame="altaz", obstime=Time.now(), location=location)
 mount = Mount(axCoord)
@@ -10,8 +11,9 @@ mount.connect()
 mount.sync_time()
 mount.stop(block=True)
 mount.calibrate_ant_coord(SkyCoord(alt=10 * u.degree, az=40 * u.degree, frame="altaz", obstime=Time.now(), location=location))
-startPos = mount.get_position()
 
+# GOTO sample
+startPos = mount.get_position()
 zenitCoord = SkyCoord(alt=89 * u.deg, az=180 * u.deg, frame="altaz", obstime=Time.now(), location=location)
 print("Current pos:", mount.get_position())
 print("Goto to zenit")
