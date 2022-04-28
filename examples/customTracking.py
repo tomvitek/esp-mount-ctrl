5e0d4160bfce817f84bb8ef98037e98356496934
Tomas Vitek <tomas.vitek.tvt@gmail.com>
2022-04-29 00:32:03 +0200

    Custom tracking resolution

--- examples/customTracking.py
+++ examples/customTracking.py
@@ -19,11 +19,15 @@ print("Generating tracking points...")
 trackPoints: List[TrackPoint] = []
 
 origTime = Time.now() + 1 * u.min
+
+alt_res = 3
+alt_start = 10
 time_per_degree = 1 * u.s
 deltaT = 0 * u.s
-for alt in range(10, 91, 10):
+
+for alt in range(alt_start, 91, alt_res):
     for az in range(90, 136):
-        if alt % 20 != 0:
+        if round((alt - alt_start) / alt_res) % 2 != 0:
             az = 90 + 135 - az        
         trackPoints.append(TrackPoint(mount.local_altaz(alt=alt * u.deg, az=az * u.deg), origTime + deltaT))
         deltaT += time_per_degree
